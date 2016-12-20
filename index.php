51c07dffd6c3f430af2f2bb4f64c31adec51e14f
Chance Smith <chancesmithb@gmail.com>
2016-12-20 16:01:35 -0600

    adds more content and header bg

--- index.php
+++ index.php
@@ -1,15 +1,29 @@
 <?php
+  //vars
+  $today = '';
+  $thisMonth = 'This Month';
   // if you must, manually put in a date
   $third_Tuesday = '';
   if($third_Tuesday === ''){
-    $third_Tuesday = new DateTime('third tuesday of this month');
+    $currMonth = date("n");
+    $currYear = date("Y");
+    $currThirdTuesday =  date("d", strtotime("third tuesday", mktime(0,0,0,$currMonth,1,$currYear)));
 
-    // if date has passed, get next month's third monday
-    if ($third_monday <= new DateTime()) {
-        $third_monday= new DateTime('third tuesday of next month');
+    if ($currThirdTuesday < date("d")){
+      // if day has past
+      $third_Tuesday = date("l, F d, Y", strtotime("third tuesday", mktime(0,0,0,$currMonth+1,1,$currYear)));
+      $thisMonth = ' Next Month';
     }
-
-    $third_Tuesday = $third_monday->format('l, F d, Y');
+    else if($currThirdTuesday == date("d")){
+      // if today
+      $third_Tuesday = date("l, F d, Y", strtotime("third tuesday", mktime(0,0,0,$currMonth,1,$currYear)));
+      $today = 'today, ';
+      $thisMonth = ' Tonight';
+    }
+    else {
+      // is a day yet to come this month
+      $third_Tuesday = date("l, F d, Y", strtotime("third tuesday", mktime(0,0,0,$currMonth,1,$currYear)));
+    }
   }
 ?>
 
@@ -19,11 +33,12 @@
   <meta charset="utf-8">
   <title>Coding Meetup for Jackson, TN | CO:de</title>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  
+  <!-- fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700|Open+Sans:400,700" rel="stylesheet">
 
   <!-- styles -->
   <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
-  <style> body {padding-top: 40px; } </style>
-
   <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
   <link rel="stylesheet" href="assets/css/main.css">
 
@@ -43,10 +58,13 @@
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
         </button>
-        <a class="navbar-brand logo" href="http://code.attheco.com">CO:de <span>@theCO</span></a>
+        <div class="navbar-brand logo">
+          <a class="brand" href="http://code.attheco.com">CO:de</a>
+          <a class="umbrella-brand">@theCO</a>
+        </div>
       </div>
       <!-- Collect the nav links, forms, and other content for toggling -->
-      <div class="collapse navbar-collapse pull-right" id="bs-example-navbar-collapse-1">
+      <div class="collapse navbar-collapse navbar-right" id="bs-example-navbar-collapse-1">
         <ul class="nav navbar-nav">
           <li>
             <a href="https://www.attheco.com/allevents/code">Meetups</a>
@@ -66,13 +84,42 @@
     </div>
     <!-- /.container -->
   </nav>
+  <div class="header-wrap">
+    <div class="container header section-space">
+      <div class="row">
+        <div class="col-md-8 col-md-offset-2 text-center">
 
-  <div class="container header">
+          <h1>Build Your Coding Skills Together</h1>
+          <p>Our next meetup is <?php echo $today; ?><?php echo $third_Tuesday; ?> @theCO.</p>
+          <a href="#" class="btn btn-default">Join Us<?php echo $thisMonth; ?></a>
+
+        </div>
+      </div>
+    </div> <!-- /container -->
+  </div>
+
+  <div class="container section-space">
     <div class="row">
-      <div class="col-md-8 col-md-offset-2 text-center">
+      <div class="col-md-6">
 
-        <h1>Build Your Coding Skills Together</h1>
-        <p>Our next meetup is <?php echo $third_Tuesday; ?>.</p>
+        <h2>Watch videos from the last meetup.</h2>
+        <p>Every month we have new instructional videos from the last meetup's coding talks.</p>
+        <a href="https://www.youtube.com/channel/UCylbtOa0TuZeW4m_wN40oBA"><img src="./assets/images/code-videos.gif" alt="code-videos" class="img-responsive"></a>
+        <p><a href="https://www.youtube.com/channel/UCylbtOa0TuZeW4m_wN40oBA" class="btn btn-default">Join Us<?php echo $thisMonth; ?></a></p>
+
+      </div>
+      <div class="col-md-6">
+
+        <h2>Web Developer Tool Built</h2>
+        <p>Here are some tools we love and encourage beginners to learn about.</p>
+        <p>
+          <a href="https://www.sublimetext.com/3" class="btn btn-default btn-outline btn-pill"><i class="fa fa-link"></i> sublime text</a>
+          <a href="https://try.github.io/levels/1/challenges/1" class="btn btn-default btn-outline btn-pill"><i class="fa fa-link"></i> git</a>
+        </p>
+
+        <h2>Want to be a CO:de Speaker?</h2>
+        <p>Show what you know, gain experience, and give back to the community.</p>
+        <p><a href="https://william10.typeform.com/to/vp9Cfo" class="btn btn-default btn-outline">Speaker Registration</a></p>
 
       </div>
     </div>
---
+++ readme.md
@@ -0,0 +1,9 @@
+#CO:de site
+
+## setup to get started
+1. `npm install`
+1. `gulp watch`
+1. get to coding
+
+##sources
+- (background header animation)[https://codepen.io/asfarmed/pen/cfslr]
