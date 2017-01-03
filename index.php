9013c67200a58b5da6cdb59c37cd337d9b9b852f
Chance Smith <chancesmithb@gmail.com>
2017-01-03 15:30:51 -0600

    favicon icon

--- index.php
+++ index.php
@@ -38,6 +38,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" type="image/x-icon" href="favicon.ico"/>
 
   <title><?php echo $title; ?></title>
   <meta name="description" content="<?php echo $description; ?>">
